b78815f4c9f569d542b04b1dc242857bf085afb7
Avinash Pandey <manikant7262@gmail.com>
2025-09-02 08:52:56 +0530

    Updated LearningOverview.tsx

--- src/components/Dashboard/LearningOverview.tsx
+++ src/components/Dashboard/LearningOverview.tsx
@@ -142,7 +142,7 @@ const LearningOverview: React.FC<LearningOverviewProps> = ({
               dataKey="points"
               stroke="f59e0b"
               fill="f59e0b"
-              fillOpacity={0.8}
+              fillOpacity={0.2}
               />
             <XAxis 
               dataKey="fullDate" 
