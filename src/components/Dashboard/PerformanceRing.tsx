2f10d0d88b73bbfaf51bc970d671d35479113ab0
Avinash Pandey <manikant7262@gmail.com>
2025-09-03 09:43:59 +0530

    Updated PerformanceRing.tsx

--- src/components/Dashboard/PerformanceRing.tsx
+++ src/components/Dashboard/PerformanceRing.tsx
@@ -49,10 +49,10 @@ const PerformanceRing: React.FC<PerformanceRingProps> = ({
   <defs>
     <linearGradient id="progressGradient" gradientTransform="rotate(90)">
       {/* <stop offset="0%" stopColor="#67b037" /> */}
-      <stop offset="0%" stopColor="#f97316" />
+      <stop offset="0%" stopColor="#67b037" />
       {/* Remaining 75% Green */}
       {/* <stop offset="25%" stopColor="#f97316" /> */}
-      <stop offset="100%" stopColor="#67b037" />
+      <stop offset="100%" stopColor="#f97316" />
     </linearGradient>
   </defs>
 
