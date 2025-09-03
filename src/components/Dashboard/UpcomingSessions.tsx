d5eab8ee1c83a43a6f5fa29914467c4f97474cc9
Avinash Pandey <manikant7262@gmail.com>
2025-09-03 07:14:06 +0530

    Updated UpcomingSessions.tsx

--- src/components/Dashboard/UpcomingSessions.tsx
+++ src/components/Dashboard/UpcomingSessions.tsx
@@ -201,7 +201,7 @@ const UpcomingSessions: React.FC<UpcomingSessionsProps> = ({
                   </div>
                 ) : (
                   <button className="w-8 h-8 bg-green-500 rounded-lg flex items-center justify-center hover:bg-blue-700 transition-colors">
-                    <span className="w-4 h-4 text-white ml-0.5" 
+                    <span className="w-4 h-4 text-xm text-white ml-0.5" 
                       onClick={(e) => {
                     e.stopPropagation();
                     navigate(`/app/courses/${encodeURIComponent(session.subject)}/theory/${encodeURIComponent(session.topic)}`);
