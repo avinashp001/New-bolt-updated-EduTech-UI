a7cdefc35ae0579a462c03496c18996107c22430
Avinash Pandey <manikant7262@gmail.com>
2025-09-03 07:58:19 +0530

    Updated WeeklyPlanTracker.tsx

--- src/components/StudyPlan/WeeklyPlanTracker.tsx
+++ src/components/StudyPlan/WeeklyPlanTracker.tsx
@@ -937,7 +937,7 @@ const WeeklyPlanTracker: React.FC = () => {
           </div>
         </div>
         
-        <div className="p-8">
+        <div className="p-6">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {(weeklyAssessments[currentWeek] || []).map((assessment, index) => (
               <div
