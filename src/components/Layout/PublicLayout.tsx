a2a5d951d040365cb78297506beca52fc466cfd8
Avinash Pandey <manikant7262@gmail.com>
2025-09-03 08:31:40 +0530

    Updated PublicLayout.tsx

--- src/components/Layout/PublicLayout.tsx
+++ src/components/Layout/PublicLayout.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import PublicHeader from './PublicHeader';
 import Footer from './Footer';
+import ScrollToTop from '../Common/ScrollToTop';
 
 const PublicLayout: React.FC = () => {
   return (
     <div className="min-h-screen bg-white">
       <PublicHeader />
+      <ScrollToTop />
       <main>
         <Outlet />
       </main>
