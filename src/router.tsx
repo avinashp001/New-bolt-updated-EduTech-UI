0ef37c96a26419357dfc1629cb77f3113b6287a2
Avinash Pandey <manikant7262@gmail.com>
2025-09-04 07:57:02 +0530

    Updated router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -66,9 +66,9 @@ const AuthLayout: React.FC = () => {
     // return <Navigate to="/app/onboarding" replace />;
     console.log('User needs onboarding, rendering OnboardingSetup directly');
     return <OnboardingSetup />;
-  } else {
+  } else if (user && user.target_exam) {
     return <EnhancedDashboard/>
-  }
+  } 
 
   
   console.log('User authenticated and onboarded, rendering main app');
